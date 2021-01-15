0487fd72ffd5a5b4d0181142447e255b618d84f8
alex <86010730@kodakalaris.com>
2021-01-14 21:45:30 -0800

    a classical button, and 2 versions of hooks; with and w/o destructuring.

--- src/components/HooksCounter.js
+++ src/components/HooksCounter.js
@@ -1,10 +1,46 @@
-import react from 'react';
+import React, {useState} from 'react';
 
+// class HooksCounter extends React.Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       counterValue: 0,
+//     }
+//   }
+//
+//   handleClick = (e) => {
+//     const { counterValue } = this.state;
+//     this.setState({counterValue: counterValue + 1})
+//   }
+//
+//   render() {
+//     const { counterValue } = this.state;
+//     return (
+//       <div>
+//         <span>{counterValue}</span>
+//         <button onClick={this.handleClick}>CLICK ME</button>
+//       </div>
+//     )
+//   }
+// }
 function HooksCounter() {
+  // const [ counterValue, setCounterValue] = useState(42)
+  // if the above line looks strange, then try doing it without...
+  //
+  const myHook = useState(42);
+  const myData = myHook[0];
+  const myUpdaterFunction = myHook[1];
+
+  const handleClick = (e) => {
+    // setCounterValue(counterValue + 1)
+    myUpdaterFunction(myData+1)
+  }
   return (
-    <span>ff</span>
+    <div>
+      <span>{myData}</span>
+      <button onClick={handleClick}>click me for a hook increment</button>
+    </div>
   )
 }
-
 
 export default HooksCounter;
